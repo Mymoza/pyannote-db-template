79f1db2940a2c3298d418fc30a694b075e3368f1
Marie-Philippe Gill <mariephilippe.gill@gmail.com>
2019-05-29 10:30:09 -0400

    feat: add db marie 0.3

--- setup.py
+++ setup.py
@@ -32,14 +32,14 @@ from setuptools import setup, find_packages
 
 setup(
     # replace "mydatabase" by the name of your database
-    name='pyannote.db.mydatabase',
+    name='pyannote.db.marie',
 
     # replace "MyDatabase" by the name of your database
-    description="MyDatabase plugin for pyannote-database",
+    description="MARIE plugin for pyannote-database",
 
     # replace with your information
-    author='Hervé Bredin',
-    author_email='bredin@limsi.fr',
+    author='Marie-Philippe Gill',
+    author_email='marie-philippe.gill.1@etsmtl.net',
 
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
@@ -47,7 +47,7 @@ setup(
 
     # replace "MyDatabase" by the new name of MyDatabase directory
     package_data={
-        'MyDatabase': [
+        'Marie': [
             'data/*',
         ],
     },
@@ -71,6 +71,6 @@ setup(
     # replace MyDatabase by the name of your database (using CamelCase)
     entry_points="""
         [pyannote.database.databases]
-        MyDatabase=MyDatabase:MyDatabase
+        Marie=Marie:Marie
     """
 )
